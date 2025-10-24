8a46989bf29c6334033c25b64eb487f8ab2bc345
devonaustin042-cpu <devonaustin042@gmail.com>
2025-10-24 18:23:27 +0700

    Add Discover page

---
+++ discover/pages/discover.php
@@ -0,0 +1,456 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="discover.css">
+    <title>Discover</title>
+</head>
+
+<body>
+
+    <header class="header">
+
+
+        <div class="dalam-header">
+
+            <img src="../../discover/asset/LOGO-goaltime.png" alt="" class="logo">
+
+            <div class="tulisan-header">
+               <h2><a href="../../homepage/Pages/homepage.php">Home</a></h2>
+               <h2><a href="../../discover/pages/discover.php">Discover</a></h2>
+               <h2><a href="../../testimoni/pages/index.php">Testimoni</a></h2>
+               <h2><a href="../../about-us/pages/about-us.php">About Us</a></h2>
+            </div>
+
+
+            </a>
+
+        </div>
+
+    </header>
+
+    <img src="../asset/hero-pctr.jpg" alt="" class="foto-utama">
+
+
+
+
+    <h1 style="font-size: 70px; margin-left: 110px; margin-top: 100px;">Sewa Lapangan</h1>
+
+    <div class="tombol-cari-lapangan">
+
+        <div class="search-container">
+
+            <img src="../asset/search-interface-symbol.png" alt="" style="width: 40px; height: 40px; ">
+            <input type="search" placeholder="Cari Nama Lapangan" class="search-input">
+
+        </div>
+
+        <div class="pilih-kota">
+            <div class="dalam-pilih-kota">
+                <img src="../asset/location.png" alt=""
+                    style="width: 40px; height: 40px; margin-left: 20px; margin-right: 10px;">
+                <p style="font-size: 21px; font-weight: 600;">Pilih kota</p>
+            </div>
+        </div>
+
+
+        <button class="cari-venue">
+            Cari Venue
+        </button>
+    </div>
+
+
+    <div class="foto-lapangan-futsal">
+
+        <div class="baris-1">
+
+            <div class="lapangan-1">
+
+                <img src="../asset/Generasi lama.jpg" alt=""
+                    style="width: 450px; height: 250px; border-top-left-radius: 40px; border-top-right-radius: 40px;">
+
+                <h1 style="margin-left: 20px; font-size: 38px;">Generasi Lama</h1>
+
+                <div class="judul-baris-1">
+                    <img src="../asset/bintang-removebg-preview.png" alt="" ; style="width: 20px; height: 20px;">
+                    <p>4.68</p>
+                    <div
+                        style="color: rgb(255, 111, 0); height: 6px; width: 6px; border-radius: 100%; background-color: rgb(255, 111, 0);">
+                    </div>
+                    <p>Kota Jaya Pura</p>
+                </div>
+
+                <div class="judul-baris-2">
+                    <h3>Mulai</h3>
+                    <h1>Rp 50.000</h1>
+                    <p>/sesi</p>
+                </div>
+
+                <div class="bagian-tombol-pesan">
+                    <hr
+                        style="width: 380px; color: rgb(255, 111, 0); border-style: solid; border-width: 3px; border-radius: 40px;">
+
+                    <div class="tombol-pesan" style="margin-top: 15px;">
+                        <h1>Pesan</h1>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div class="lapangan-1">
+
+                <img src="../asset/Futsal corner bekasi.jpg" alt=""
+                    style="width: 450px; height: 250px; border-top-left-radius: 40px; border-top-right-radius: 40px;">
+
+                <h1 style="margin-left: 20px; font-size: 38px;">Futsal Corner Bekasi</h1>
+
+                <div class="judul-baris-1">
+                    <img src="../asset/bintang-removebg-preview.png" alt="" ; style="width: 20px; height: 20px;">
+                    <p>4.10</p>
+                    <div
+                        style="color: rgb(255, 111, 0); height: 6px; width: 6px; border-radius: 100%; background-color: rgb(255, 111, 0);">
+                    </div>
+                    <p>Kota Bekasi</p>
+                </div>
+
+                <div class="judul-baris-2">
+                    <h3>Mulai</h3>
+                    <h1>Rp 100.000</h1>
+                    <p>/sesi</p>
+                </div>
+
+                <div class="bagian-tombol-pesan">
+                    <hr
+                        style="width: 380px; color: rgb(255, 111, 0); border-style: solid; border-width: 3px; border-radius: 40px;">
+
+                    <div class="tombol-pesan" style="margin-top: 15px;">
+                        <h1>Pesan</h1>
+                    </div>
+                </div>
+
+            </div>
+
+
+
+            <div class="lapangan-1">
+
+                <img src="../asset/Futsal kebon jeruk.jpg" alt=""
+                    style="width: 450px; height: 250px; border-top-left-radius: 40px; border-top-right-radius: 40px;">
+
+                <h1 style="margin-left: 20px; font-size: 38px;">Futsal Kebon Jeruk</h1>
+
+                <div class="judul-baris-1">
+                    <img src="../asset/bintang-removebg-preview.png" alt="" ; style="width: 20px; height: 20px;">
+                    <p>4.75</p>
+                    <div
+                        style="color: rgb(255, 111, 0); height: 6px; width: 6px; border-radius: 100%; background-color: rgb(255, 111, 0);">
+                    </div>
+                    <p>Kota Balikpapan</p>
+                </div>
+
+                <div class="judul-baris-2">
+                    <h3>Mulai</h3>
+                    <h1>Rp 80.000</h1>
+                    <p>/sesi</p>
+                </div>
+
+                <div class="bagian-tombol-pesan">
+                    <hr
+                        style="width: 380px; color: rgb(255, 111, 0); border-style: solid; border-width: 3px; border-radius: 40px;">
+
+                    <div class="tombol-pesan" style="margin-top: 15px;">
+                        <h1>Pesan</h1>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+
+        <div class="baris-1">
+
+            <div class="lapangan-1">
+
+                <img src="../asset/Futsal limbad.jpg" alt=""
+                    style="width: 450px; height: 250px; border-top-left-radius: 40px; border-top-right-radius: 40px;">
+
+                <h1 style="margin-left: 20px; font-size: 38px;">Futsal limbad</h1>
+
+                <div class="judul-baris-1">
+                    <img src="../asset/bintang-removebg-preview.png" alt="" ; style="width: 20px; height: 20px;">
+                    <p>4.88</p>
+                    <div
+                        style="color: rgb(255, 111, 0); height: 6px; width: 6px; border-radius: 100%; background-color: rgb(255, 111, 0);">
+                    </div>
+                    <p>Kota Jakarta Barat</p>
+                </div>
+
+                <div class="judul-baris-2">
+                    <h3>Mulai</h3>
+                    <h1>Rp 70.000</h1>
+                    <p>/sesi</p>
+                </div>
+
+                <div class="bagian-tombol-pesan">
+                    <hr
+                        style="width: 380px; color: rgb(255, 111, 0); border-style: solid; border-width: 3px; border-radius: 40px;">
+
+                    <div class="tombol-pesan" style="margin-top: 15px;">
+                        <h1>Pesan</h1>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div class="lapangan-1">
+
+                <img src="../asset/futsal rooftop.jpg" alt=""
+                    style="width: 450px; height: 250px; border-top-left-radius: 40px; border-top-right-radius: 40px;">
+
+                <h1 style="margin-left: 20px; font-size: 38px;">Futsal Rooftop</h1>
+
+                <div class="judul-baris-1">
+                    <img src="../asset/bintang-removebg-preview.png" alt="" ; style="width: 20px; height: 20px;">
+                    <p>4.95</p>
+                    <div
+                        style="color: rgb(255, 111, 0); height: 6px; width: 6px; border-radius: 100%; background-color: rgb(255, 111, 0);">
+                    </div>
+                    <p>Kota Singkawang</p>
+                </div>
+
+                <div class="judul-baris-2">
+                    <h3>Mulai</h3>
+                    <h1>Rp 90.000</h1>
+                    <p>/sesi</p>
+                </div>
+
+                <div class="bagian-tombol-pesan">
+                    <hr
+                        style="width: 380px; color: rgb(255, 111, 0); border-style: solid; border-width: 3px; border-radius: 40px;">
+
+                    <div class="tombol-pesan" style="margin-top: 15px;">
+                        <h1>Pesan</h1>
+                    </div>
+                </div>
+
+            </div>
+
+
+
+
+            <div class="lapangan-1">
+
+                <img src="../asset/danau cintarum.jpg" alt=""
+                    style="width: 450px; height: 250px; border-top-left-radius: 40px; border-top-right-radius: 40px;">
+
+                <h1 style="margin-left: 20px; font-size: 38px;">Danau Cintarum</h1>
+
+                <div class="judul-baris-1">
+                    <img src="../asset/bintang-removebg-preview.png" alt="" ; style="width: 20px; height: 20px;">
+                    <p>4.90</p>
+                    <div
+                        style="color: rgb(255, 111, 0); height: 6px; width: 6px; border-radius: 100%; background-color: rgb(255, 111, 0);">
+                    </div>
+                    <p>Kota Banda Aceh</p>
+                </div>
+
+                <div class="judul-baris-2">
+                    <h3>Mulai</h3>
+                    <h1>Rp 120.000</h1>
+                    <p>/sesi</p>
+                </div>
+
+                <div class="bagian-tombol-pesan">
+                    <hr
+                        style="width: 380px; color: rgb(255, 111, 0); border-style: solid; border-width: 3px; border-radius: 40px;">
+
+                    <div class="tombol-pesan" style="margin-top: 15px;">
+                        <h1>Pesan</h1>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+        <div class="baris-1">
+
+            <div class="lapangan-1">
+
+                <img src="../asset/sports corner.jpg" alt=""
+                    style="width: 450px; height: 250px; border-top-left-radius: 40px; border-top-right-radius: 40px;">
+
+                <h1 style="margin-left: 20px; font-size: 38px;">Sports Corner</h1>
+
+                <div class="judul-baris-1">
+                    <img src="../asset/bintang-removebg-preview.png" alt="" ; style="width: 20px; height: 20px;">
+                    <p>4.50</p>
+                    <div
+                        style="color: rgb(255, 111, 0); height: 6px; width: 6px; border-radius: 100%; background-color: rgb(255, 111, 0);">
+                    </div>
+                    <p>Kota Jakarta Pusat</p>
+                </div>
+
+                <div class="judul-baris-2">
+                    <h3>Mulai</h3>
+                    <h1>Rp 50.000</h1>
+                    <p>/sesi</p>
+                </div>
+
+                <div class="bagian-tombol-pesan">
+                    <hr
+                        style="width: 380px; color: rgb(255, 111, 0); border-style: solid; border-width: 3px; border-radius: 40px;">
+
+                    <div class="tombol-pesan" style="margin-top: 15px;">
+                        <h1>Pesan</h1>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div class="lapangan-1">
+
+                <img src="../asset/futsal hijau indah.jpg" alt=""
+                    style="width: 450px; height: 250px; border-top-left-radius: 40px; border-top-right-radius: 40px;">
+
+                <h1 style="margin-left: 20px; font-size: 38px;">Futsal Hijau Indah</h1>
+
+                <div class="judul-baris-1">
+                    <img src="../asset/bintang-removebg-preview.png" alt="" ; style="width: 20px; height: 20px;">
+                    <p>3.05</p>
+                    <div
+                        style="color: rgb(255, 111, 0); height: 6px; width: 6px; border-radius: 100%; background-color: rgb(255, 111, 0);">
+                    </div>
+                    <p>Kota Pontianak</p>
+                </div>
+
+                <div class="judul-baris-2">
+                    <h3>Mulai</h3>
+                    <h1>Rp 70.000</h1>
+                    <p>/sesi</p>
+                </div>
+
+                <div class="bagian-tombol-pesan">
+                    <hr
+                        style="width: 380px; color: rgb(255, 111, 0); border-style: solid; border-width: 3px; border-radius: 40px;">
+
+                    <div class="tombol-pesan" style="margin-top: 15px;">
+                        <h1>Pesan</h1>
+                    </div>
+                </div>
+
+            </div>
+
+
+
+
+            <div class="lapangan-1">
+
+                <img src="../asset/arena balap.jpg" alt=""
+                    style="width: 450px; height: 250px; border-top-left-radius: 40px; border-top-right-radius: 40px;">
+
+                <h1 style="margin-left: 20px; font-size: 38px;">Arena Balap</h1>
+
+                <div class="judul-baris-1">
+                    <img src="../asset/bintang-removebg-preview.png" alt="" ; style="width: 20px; height: 20px;">
+                    <p>4.80</p>
+                    <div
+                        style="color: rgb(255, 111, 0); height: 6px; width: 6px; border-radius: 100%; background-color: rgb(255, 111, 0);">
+                    </div>
+                    <p>Kota Singkawang</p>
+                </div>
+
+                <div class="judul-baris-2">
+                    <h3>Mulai</h3>
+                    <h1>Rp 90.000</h1>
+                    <p>/sesi</p>
+                </div>
+
+                <div class="bagian-tombol-pesan">
+                    <hr
+                        style="width: 380px; color: rgb(255, 111, 0); border-style: solid; border-width: 3px; border-radius: 40px;">
+
+                    <div class="tombol-pesan" style="margin-top: 15px;">
+                        <h1>Pesan</h1>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="tombol-load-more" style="margin-top: 15px;">
+        <h1>Load More</h1>
+    </div>
+    
+
+    <footer class="footer">
+        <div class="footer-container">
+
+
+            <div class="footer-section footer-about">
+                <div class="logo-aboutus">
+                    <img src="../asset//LOGO-goaltime.png" alt="GoalTime Logo" class="footer-logo"
+                        style="margin-top: -38px;">
+                    <p style="margin-top: -51px;">
+                        Platform reservasi futsal secara online untuk yang <br>memudahkan Anda
+                        dalam memesan lapangan <br> dengan mudah, cepat, dan praktis
+                    </p>
+                </div>
+            </div>
+
+            <div class="footer-section">
+                <h4>Company</h4>
+                <ul>
+                    <li><a href="#">Tentang Kami</a></li>
+                    <li><a href="#">Layanan</a></li>
+                    <li><a href="#">Testimoni</a></li>
+                    <li><a href="#">Blog</a></li>
+                </ul>
+            </div>
+
+
+            <div class="footer-section">
+                <h4>Support</h4>
+                <ul>
+                    <li><a href="#">Pusat Bantuan</a></li>
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">Cara reservasi</a></li>
+                    <li><a href="#">Feedback</a></li>
+                </ul>
+            </div>
+
+
+            <div class="footer-section">
+                <h4>Contact Us</h4>
+                <ul>
+                    <li>📞 +62. 684189547</li>
+                    <li>📍 Alamat Lapangan</li>
+                    <li>📧 GoalTime@gmail.com</li>
+                </ul>
+            </div>
+        </div>
+
+
+        <div class="footer-bottom">
+            <p>Copyright © 2025 ReservasiFutsal. All Rights Reserved.</p>
+            <div class="footer-links">
+                <a href="#">Privacy Policy</a>
+                <span>|</span>
+                <a href="#">Terms of Use</a>
+            </div>
+        </div>
+    </footer>
+
+
+
+
+
+
+</body>
+
+</html>
