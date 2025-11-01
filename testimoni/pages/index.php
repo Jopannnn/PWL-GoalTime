def8ef49fb2dd1724baaa2f9dfc48b5db1cfdbbf
devonaustin042-cpu <devonaustin042@gmail.com>
2025-11-01 18:08:42 +0700

    add testimoni page

---
+++ testimoni/pages/index.php
@@ -0,0 +1,321 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>GoalTime</title>
+    <link rel="stylesheet" href="testimoni.css" />
+</head>
+
+<body>
+
+    <!-- NAVBAR -->
+    <header class="navbar">
+        <div class="container-nav">
+
+            <img src="../../homepage/asset/LOGO-goaltime.png" alt="GoalTime-LOGO" class="logo">
+
+            <div class="tulisan-headerrr">
+               <h2><a href="../../homepage/Pages/homepage.php">Home</a></h2>
+               <h2><a href="../../discover/pages/discover.php">Discover</a></h2>
+               <h2><a href="../../testimoni/pages/index.php">Testimoni</a></h2>
+               <h2><a href="../../about-us/pages/about-us.php">About Us</a></h2>
+            </div>
+        </div>
+    </header>
+
+    <!-- HERO -->
+    <section class="hero">
+        <img src="../asset/BANNER.png" alt="hero" />
+    </section>
+
+    <!-- TESTIMONI -->
+
+    <h2 class="testimoni-anda">Testimoni Anda</h2>
+
+    <section class="testimoni container">
+        <div class="grid-testimoni">
+            <div class="baris-kotak">
+                <div class="kotak">
+                    <div class="bagian-kiri">
+                        <img class="profil" src="../asset/Fahri.png" alt="user">
+                        <h4>Fahri, Jakarta Utara</h4>
+                    </div>
+
+                    <div class="bagian-kanan">
+
+                        <div class="tulisan-kotak">
+                            <p style="font-size: 22px; margin-right: 40px; margin-left: 40px; ">"Proses
+                                reservasinya super
+                                cepat dan
+                                mudah!
+                                Lapangannya bersih,
+                                stafnya ramah, dan sistem
+                                online-nya
+                                bikin booking jadi praktis banget."</p>
+                        </div>
+
+
+                        <div class="baris-bintang">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                        </div>
+
+
+                    </div>
+
+                </div>
+
+
+                <div class="kotak">
+                    <div class="bagian-kiri">
+                        <img class="profil" src="../asset/Olga.png" alt="user">
+                        <h4>Olga, Banda Aceh</h4>
+                    </div>
+
+                    <div class="bagian-kanan">
+
+                        <div class="tulisan-kotak">
+                            <p style="font-size: 22px; margin-right: 40px; margin-left: 40px; ">
+                                "Platform reservasi ini sangat membantu tim kami mengatur jadwal latihan. Prosesnya
+                                cepat, sistemnya jelas, dan lapangannya keren."</p>
+                        </div>
+
+
+                        <div class="baris-bintang">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                            <img class="stars" src="../asset/star putih.png" alt="stars">
+                        </div>
+
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="baris-kotak">
+                <div class="kotak">
+                    <div class="bagian-kiri">
+                        <img class="profil" src="../asset/Simajuntak.png" alt="user">
+                        <h4>Simanjuntak, Bogor</h4>
+                    </div>
+
+                    <div class="bagian-kanan">
+
+                        <div class="tulisan-kotak">
+                            <p style="font-size: 22px; margin-right: 40px; margin-left: 40px; ">“Reservasi di
+                                website
+                                ini sangat mudah dan
+                                tidak ribet dan sistem pembayaran yang simple.”</p>
+                        </div>
+
+
+                        <div class="baris-bintang">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                            <img class="stars" src="../asset/star putih.png" alt="stars">
+                        </div>
+
+
+                    </div>
+
+                </div>
+
+                <div class="kotak">
+                    <div class="bagian-kiri">
+                        <img class="profil" src="../asset/Asep.png" alt="user">
+                        <h4>Asep, Pontianak</h4>
+                    </div>
+
+                    <div class="bagian-kanan">
+
+                        <div class="tulisan-kotak">
+                            <p style="font-size: 22px; margin-right: 40px; margin-left: 40px; ">“Reservasinya
+                                gampang
+                                dan cepat dan lapangan selalu ready dan bersih.”</p>
+                        </div>
+
+
+                        <div class="baris-bintang">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                        </div>
+
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="baris-kotak">
+                <div class="kotak">
+                    <div class="bagian-kiri">
+                        <img class="profil" src="../asset/Wibi.png" alt="user">
+                        <h4>Wibi, Bali</h4>
+                    </div>
+
+                    <div class="bagian-kanan">
+
+                        <div class="tulisan-kotak">
+                            <p style="font-size: 22px; margin-right: 40px; margin-left: 40px; ">"Tim kami rutin
+                                pakai
+                                layanan ini buat latihan mingguan. Reservasi lancar, lapangan nyaman, dan jadwal
+                                selalu
+                                on time."</p>
+                        </div>
+
+
+                        <div class="baris-bintang">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                        </div>
+
+
+                    </div>
+
+                </div>
+
+                <div class="kotak">
+                    <div class="bagian-kiri">
+                        <img class="profil" src="../asset/Andi.png" alt="user">
+                        <h4>Andi, Bandung</h4>
+                    </div>
+
+                    <div class="bagian-kanan">
+
+                        <div class="tulisan-kotak">
+                            <p style="font-size: 22px; margin-right: 40px; margin-left: 40px; ">"Sistem reservasinya
+                                efisien, lapangannya terawat, dan jadwal selalu tepat. Cocok buat saya yang nggak
+                                suka
+                                ribet."</p>
+                        </div>
+
+
+                        <div class="baris-bintang">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                            <img class="stars" src="../asset/Star 1.png" alt="stars">
+                        </div>
+
+
+                    </div>
+
+                </div>
+            </div>
+
+
+            <div class="kotak-load-more">
+                <button class="btn-orange">Load More</button>
+            </div>
+
+
+        </div>
+
+
+    </section>
+
+    <!-- PENDAPAT ATLET -->
+    <section class="atlet">
+
+        <h1>Pendapat Atlet Profesional</h1>
+
+        <div class="container-atlet">
+            <div class="atlet-img">
+                <img src="../asset/Evanmovic.png" alt="athlete">
+                <h3>Evan Septinus Soumilena <br>Fafage Benua Player</h3>
+            </div>
+            <div class="atlet-text">
+                <p>"Pengalaman saya bertahun-tahun di dunia futsal membuat saya tahu betul kualitas lapangan yang baik.
+                    Saya tidak menyangka bisa menemukan tempat booking online yang begitu efisien. Lapangan di sini
+                    sangat terawat, dari kondisi rumput sampai kebersihan ruang gantinya. Pelayanan timnya juga ramah
+                    dan sangat membantu. Ini bukan cuma tempat main futsal, tapi juga tempat di mana kita bisa berlatih
+                    dengan maksimal.</p>
+            </div>
+        </div>
+    </section>
+
+
+
+
+    <!-- footer -->
+    <footer class="footer">
+        <div class="footer-container">
+
+
+            <div class="footer-section footer-about">
+                <div class="logo-aboutus">
+                    <img src="../../homepage/asset/LOGO-goaltime.png" alt="GoalTime Logo" class="footer-logo"
+                        style="margin-top: -38px;">
+                    <p style="margin-top: -51px;">
+                        Platform reservasi futsal secara online untuk yang <br>memudahkan Anda
+                        dalam memesan lapangan <br> dengan mudah, cepat, dan praktis
+                    </p>
+                </div>
+            </div>
+
+
+            <div class="footer-section">
+                <h4>Company</h4>
+                <ul>
+                    <li><a href="../../about-us/pages/about-us.php">Tentang Kami</a></li>
+                    <li><a href="#">Layanan</a></li>
+                    <li><a href="../../testimoni/pages/index.php">Testimoni</a></li>
+                    <li><a href="#">Blog</a></li>
+                </ul>
+            </div>
+
+
+            <div class="footer-section">
+                <h4>Support</h4>
+                <ul>
+                    <li><a href="#">Pusat Bantuan</a></li>
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">Cara reservasi</a></li>
+                    <li><a href="#">Feedback</a></li>
+                </ul>
+            </div>
+
+
+            <div class="footer-section">
+                <h4>Contact Us</h4>
+                <ul>
+                    <li>📞 +62. 684189547</li>
+                    <li>📍 Alamat Lapangan</li>
+                    <li>📧 GoalTime@gmail.com</li>
+                </ul>
+            </div>
+        </div>
+
+
+        <div class="footer-bottom">
+            <p>Copyright © 2025 ReservasiFutsal. All Rights Reserved.</p>
+            <div class="footer-links">
+                <a href="#">Privacy Policy</a>
+                <span>|</span>
+                <a href="#">Terms of Use</a>
+            </div>
+        </div>
+    </footer>
+
+
+
+</body>
+
+</html>
